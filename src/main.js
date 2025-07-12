b8c0a06189adcdac6b31606fae95c1fb7be29fa1
ronnydp <ronnydp742@gmail.com>
2025-07-12 02:45:47 -0500

    show next question

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { preguntas } from "./data";
 
 const pregunta = document.getElementById("pregunta");
 const options = document.querySelectorAll(".options");
-const siguiente = document.querySelector(".button")
+const siguiente = document.querySelector(".button");
+const numero_pregunta = document.getElementById("orden")
 
 pregunta.innerText = preguntas[0].pregunta;
 
@@ -11,6 +12,11 @@ for (let i = 0; i < preguntas[0].opciones.length; i++) {
         <label for="op${i}" id="op${i}">${preguntas[0].opciones[i]}</label>`;
 }
 
-siguiente.addEventListener( 'click', () => {
-    alert("funciona")
-})
+siguiente.addEventListener("click", () => {
+  pregunta.innerText = preguntas[1].pregunta;
+  for (let i = 0; i < preguntas[0].opciones.length; i++) {
+    options[i].innerHTML = `<input type="radio" name="options" value="op${i}" />
+              <label for="op${i}" id="op${i}">${preguntas[1].opciones[i]}</label>`;
+  }
+  numero_pregunta.innerText = 'Pregunta 2';
+});
